f16568229ea1638c5f4761f25d67820ebed4486f
Brayan <brayan.vaccaro@studio.unibo.it>
2022-12-17 16:32:26 +0100

    modified css, added login file

---
+++ src/pages/login.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <div id="login">
+        <section>
+
+            <th>Username</th>
+            <input type="text" placeholder="username">
+
+            <th>Password</th>
+            <input type="text" placeholder="password">
+
+
+            <!-- forse questo diventerà un nuxtlink -->
+            <button>INIZIA QUI</button> 
+
+
+        </section>
+    </div>
+</template>
