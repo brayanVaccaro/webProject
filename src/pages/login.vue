fe0a763e3b515eb9cbfd037d6e2956fe980da3ad
Brayan <brayan.vaccaro@studio.unibo.it>
2022-12-18 14:18:50 +0100

    improved login page with better css

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,54 +1,135 @@
 <template>
-    <div>
-        <section id="login">
-            <h1>LOGIN</h1>
-            <div class="flexbox-login">
-                <b>Username</b>
-                <input type="text" placeholder="username">
+    <section>
+        <div class="enter">
+            <input type="checkbox" id="chk" aria-hidden="true">
 
-                <b>Password</b>
-                <input type="text" placeholder="password">
-                <button id="invioLogin">
-                    <NuxtLink to="/">INIZIA QUI</NuxtLink>
-
-                </button>
-                <section id="notRegistered">
-                    <a href="/">Non sei ancora registrato?</a>
-                </section>
+            <div class="signup">
+                <form>
+                    <label for="chk" aria-hidden="true">Sign up</label>
+                    <input type="text" name="txt" placeholder="User name" required="true">
+                    <input type="email" name="email" placeholder="Email" required="true">
+                    <input type="password" name="pswd" placeholder="Password" required="true">
+                    <button>Sign up</button>
+                </form>
             </div>
 
-        </section>
-    </div>
+            <div class="login">
+                <form>
+                    <label for="chk" aria-hidden="true">Login</label>
+                    <input type="email" name="email" placeholder="Email" required="">
+                    <input type="password" name="pswd" placeholder="Password" required="">
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
+body {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    font-family: 'Jost', sans-serif;
+    background: linear-gradient(to bottom, #0f0c29, #302b63, #24243e);
+}
+section {
+    display: flex;
+    justify-content: center;
+    margin: 2% auto;
+}
+.enter {
+    width: 350px;
+    height: 500px;
+    background: red;
+    overflow: hidden;
+    // background: 
+    border-radius: 10px;
+    box-shadow: 5px 20px 50px #000;
 
-h1 {
-    text-align: center;
-    margin: 1%;
 }
 
+#chk {
+    display: none;
+}
 
+.signup {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+
+label {
+    color: #fff;
+    font-size: 2.3em;
+    justify-content: center;
+    display: flex;
+    margin: 60px;
+    font-weight: bold;
+    cursor: pointer;
+    transition: .5s ease-in-out;
+}
 
 input {
-    margin: 0;
-}
-
-.flexbox-login {
-    padding: 0 25%;
-    margin: 1% 0% 1% 1%;
+    width: 60%;
+    height: 20px;
+    background: #e0dede;
+    justify-content: center;
     display: flex;
-    align-items: flex-start;
-    flex-direction: column;
+    margin: 20px auto;
+    padding: 10px;
+    border: none;
+    outline: none;
+    border-radius: 5px;
 }
 
-#invioLogin {
+button {
+    width: 60%;
+    height: 40px;
+    margin: 10px auto;
+    justify-content: center;
     display: block;
-    width: 30%;
-    margin: 1% 0;
+    color: #fff;
+    background: #573b8a;
+    font-size: 1em;
+    font-weight: bold;
+    margin-top: 20px;
+    outline: none;
+    border: none;
+    border-radius: 5px;
+    transition: .2s ease-in;
+    cursor: pointer;
 }
 
-#notRegistered {
-    padding: 0;
+button:hover {
+    background: #6d44b8;
+}
+
+.login {
+    height: 460px;
+    background: #eee;
+    border-radius: 60% / 10%;
+    transform: translateY(-180px);
+    transition: .8s ease-in-out;
+}
+
+.login label {
+    color: #573b8a;
+    transform: scale(.6);
+}
+
+#chk:checked~.login {
+    transform: translateY(-500px);
+}
+
+#chk:checked~.login label {
+    transform: scale(1);
+}
+
+#chk:checked~.signup label {
+    transform: scale(.6);
 }
 </style>
