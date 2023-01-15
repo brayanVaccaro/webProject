b941019223de5bdd9fd7e1cf19a4bde7a60fb84a
Marawan <marawan.emadragab@studio.unibo.it>
2023-01-15 20:05:36 +0100

    changes on review page

--- src/server/api/reviews/getReviews.ts
+++ src/server/api/reviews/getReviews.ts
@@ -1,12 +1,10 @@
 import { createConnection } from "~~/src/server/utils/db";
 
-export default defineEventHandler(async function (event) {
-
-   // const { tagliaStanza } = await readBody(event)
+export default defineEventHandler(async function () {
 
    const connection = await createConnection()
    const [results] = await connection.execute(
-      `SELECT * FROM recensione INNER JOIN utenti on utenti.idUtente != recensione.idUtente WHERE `,
+      `SELECT * FROM recensione`
       
    )
 
